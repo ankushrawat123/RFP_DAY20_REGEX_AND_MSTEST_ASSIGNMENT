d908cb5bc6738b44872021b120b6da3db3273fb4
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 01:14:29 +0530

    Uploading EmailSample file in Uc9 branch

--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
 Console.WriteLine("Welcome to REGEX PROGRAM!\n");
-Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n4 FOR VALIDATE MOBILE REGEX PROGRAM \n5 FOR VALIDATE PASSWORD MINIMUM 8 CHARACTERS REGEX PROGRAM\n6 FOR VALIDATE PASSWORD ATLEAST 1 UPPERCASE CHARACTERS REGEX PROGRAM\n7 FOR VALIDATE PASSWORD ATLEAST 1 NUMERIC CHARACTERS REGEX PROGRAM\n8 FOR VALIDATE PASSWORD ATLEAST 1 SPECIAL CHARACTERS REGEX PROGRAM\n");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n4 FOR VALIDATE MOBILE REGEX PROGRAM \n5 FOR VALIDATE PASSWORD MINIMUM 8 CHARACTERS REGEX PROGRAM\n6 FOR VALIDATE PASSWORD ATLEAST 1 UPPERCASE CHARACTERS REGEX PROGRAM\n7 FOR VALIDATE PASSWORD ATLEAST 1 NUMERIC CHARACTERS REGEX PROGRAM\n8 FOR VALIDATE PASSWORD ATLEAST 1 SPECIAL CHARACTERS REGEX PROGRAM\n9 FOR VALIDATE EMAIL SAMPLE REGEX PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -89,15 +89,46 @@ switch (num)
         }
         break;
 
+
     case 8:
         PWD_Special_Char Min1SpecialCharObj = new PWD_Special_Char();
-        if (Min1SpecialCharObj.one_SpecialChar_Pwd("AnkushRaw4u@"))
+        if (Min1SpecialCharObj.one_SpecialChar_Pwd("AnkushRA4u@"))
         {
             Console.WriteLine("Valid");
         }
         else
         {
             Console.WriteLine("Invalid");
+        }
+        break;
+
+    case 9:
+        EmailSamples EmailSamplesObj = new EmailSamples();
+        string[] EmailArray = new string[]
+        {
+          "abc@yahoo.com",
+          "abc-100@yahoo.com",
+          "abc.100@yahoo.com",
+          "abc111@abc.com",
+          "abc-100@abc.net",
+          "abc.100@abc.com.au",
+          "abc@1.com",
+          "abc@gmail.com.com",
+          "abc+100@gmail.com"
+        };
+
+        foreach(string i in EmailArray)
+        {
+
+        if (EmailSamplesObj.emailSample(i))
+        {
+            Console.WriteLine(i+" is Valid");
+        }
+        else
+        {
+            Console.WriteLine(i+" is Invalid");
+        }
+
         }
         break;
 
