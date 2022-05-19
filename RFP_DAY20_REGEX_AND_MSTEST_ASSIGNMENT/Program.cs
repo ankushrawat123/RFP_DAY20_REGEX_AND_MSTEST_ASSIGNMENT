0001b20078b2d3926a6735579303da551edf98de
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 00:38:47 +0530

    Uploading Validate email file in Uc3 branch

--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
 Console.WriteLine("Welcome to REGEX PROGRAM!\n");
-Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -19,7 +19,19 @@ switch (num)
 
     case 2:
         ValidFirstName lastNameObj = new ValidFirstName();
-        if (lastNameObj.Validate_Name("Rawat"))
+        if (lastNameObj.Validate_Name("rawat"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
+    case 3:
+        ValidateEmail EmailObj = new ValidateEmail();
+        if (EmailObj.Validate_Email("abc.xyz@bl.co.in"))
         {
             Console.WriteLine("Valid");
         }
