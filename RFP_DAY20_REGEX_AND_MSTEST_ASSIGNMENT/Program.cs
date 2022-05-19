823b06c38fd0e72f0fedff27bb56c4369a1b4067
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 00:34:37 +0530

    Uploading ValidLastName file in Uc2 branch

--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,13 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
-Console.WriteLine("Welcome to VALIDATE FIRSTNAME REGEX PROGRAM!\n");
-ValidFirstName nameObj = new ValidFirstName();
-if (nameObj.Validate_Name("Ankush"))
+Console.WriteLine("Welcome to REGEX PROGRAM!\n");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
 {
-Console.WriteLine("Valid");
+    case 1:
+        ValidFirstName nameObj = new ValidFirstName();
+        if (nameObj.Validate_Name("Ankush"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
+    case 2:
+        ValidFirstName lastNameObj = new ValidFirstName();
+        if (lastNameObj.Validate_Name("Rawat"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
 }
-else
-{
-Console.WriteLine("Invalid");
-}
-        
