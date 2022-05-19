6aeab55b5ef5c96ed5ef72661daf6dfb75e73803
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 00:51:03 +0530

    Uploading PasswordWith1MinUpperCase file in UC6

---
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/PasswordMin1UpperCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT
+{
+    internal class PasswordMin1UpperCase
+    {
+        public String Regex_Pwd_1UpperCase = "^(?=.*[A-Z])[A-Z a-z 0-9 $#@!&*?|]{8,}$";
+        public bool PwdUpperCase(string PwdUpperCase)
+        {
+            return Regex.IsMatch(PwdUpperCase, Regex_Pwd_1UpperCase);
+        }
+    }
+}
--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
 Console.WriteLine("Welcome to REGEX PROGRAM!\n");
-Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n4 FOR VALIDATE MOBILE REGEX PROGRAM \n5 FOR VALIDATE PASSWORD MINIMUM 8 CHARACTERS REGEX PROGRAM\n");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n4 FOR VALIDATE MOBILE REGEX PROGRAM \n5 FOR VALIDATE PASSWORD MINIMUM 8 CHARACTERS REGEX PROGRAM\n6 FOR VALIDATE PASSWORD ATLEAST 1 UPPERCASE CHARACTERS REGEX PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -56,6 +56,18 @@ switch (num)
     case 5:
         ValidatePassword PasswordObj = new ValidatePassword();
         if(PasswordObj.Password("%Ankush4u344&"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
+    case 6:
+        PasswordMin1UpperCase PwdUpperObj = new PasswordMin1UpperCase();
+        if(PwdUpperObj.PwdUpperCase("Ankush234"))
         {
             Console.WriteLine("Valid");
         }
