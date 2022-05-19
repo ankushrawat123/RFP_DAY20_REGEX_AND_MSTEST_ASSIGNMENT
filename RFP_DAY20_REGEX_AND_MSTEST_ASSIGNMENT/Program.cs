a55109000b0335fe5f85c675148e8e92bda168db
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 00:29:33 +0530

    Uploading ValidateFirstName file in Uc1

--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");  
+using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
+Console.WriteLine("Welcome to VALIDATE FIRSTNAME REGEX PROGRAM!\n");
+ValidFirstName nameObj = new ValidFirstName();
+if (nameObj.Validate_Name("Ankush"))
+{
+Console.WriteLine("Valid");
+}
+else
+{
+Console.WriteLine("Invalid");
+}
+        
---
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/ValidFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT
+{
+    class ValidFirstName
+    {
+        public static string Regex_Name = "^[A-Z][A-Z a-z ]{3,}$";//First character in Upper Case and minimum 3 characters
+        public bool Validate_Name(string Name)
+        {
+         return Regex.IsMatch(Name, Regex_Name);
+        }
+    }
+}
