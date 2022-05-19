6c27d1425df9507ca76b7f2cf8fda96fdfef3704
AnkushRawat <ankushrwt344@gmail.com>
2022-05-20 00:42:14 +0530

    Uploading ValidateMobile in Uc3 branch

--- RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
+++ RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY20_REGEX_AND_MSTEST_ASSIGNMENT;
 Console.WriteLine("Welcome to REGEX PROGRAM!\n");
-Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n");
+Console.WriteLine("ENTER \n1 FOR VALIDATE FIRSTNAME REGEX PROGRAM \n2 FOR VALIDATE LASTNAME REGEX PROGRAM\n3 FOR VALIDATE EMAIL REGEX PROGRAM\n4 FOR VALIDATE MOBILE REGEX PROGRAM\n");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -32,6 +32,18 @@ switch (num)
     case 3:
         ValidateEmail EmailObj = new ValidateEmail();
         if (EmailObj.Validate_Email("abc.xyz@bl.co.in"))
+        {
+            Console.WriteLine("Valid");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        break;
+
+    case 4:
+        ValidateMobile MobileObj = new ValidateMobile();    
+        if(MobileObj.Validate_Mobile("91 8734233456"))
         {
             Console.WriteLine("Valid");
         }
