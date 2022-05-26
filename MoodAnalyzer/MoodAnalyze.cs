6fd10e1988d498dcbf161d0719bbfe979ff3d3a0
AnkushRawat <ankushrwt344@gmail.com>
2022-05-26 18:24:36 +0530

    Uploading MoodAnalyzer_Uc1 file in Uc1 branch

---
+++ MoodAnalyzer/MoodAnalyze.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyser
+    {
+        string Message;
+       public MoodAnalyser()
+        {
+
+        }
+        public MoodAnalyser(string message)
+        {
+            this.Message = message;
+        }
+        public string analyseMood()
+        {
+            if(this.Message.ToUpper().Contains("HAPPY"))
+            {
+                return "HAPPY";
+            }
+            if (this.Message.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            return "HAPPY";
+        }
+    }
+}
---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MoodAnalyzer;
+Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAM ");
+MoodAnalyser mood = new MoodAnalyser("I am feeling happy");
+string result = mood.analyseMood();
+Console.WriteLine(result);
