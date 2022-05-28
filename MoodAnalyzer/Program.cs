f5bd1cc773abc9beac4663ee99e762c54ad60998
AnkushRawat <ankushrwt344@gmail.com>
2022-05-28 15:51:28 +0530

    Uploading MoodAnalyzer invokeProgram in Uc6 branch

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MoodAnalyzer;
 Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAM ");
-Console.WriteLine("Enter \n1 FOR MOODANALYZER SAD,HAPPY & OTHER MOOD PROGRAM \n2 FOR MOODANALYZER INVALID INPUT PROGRAM\n3 FOR CUSTOM EXCEPTION PROGRAM\n4 FOR MOODANALYZER RETURN OBJECT PROGRAM\n5 FOR MOODANALYZER RETURN OBJECT WITH CONSTRUCTOR USING REFLECTION PROGRAM\n ");
+Console.WriteLine("Enter \n1 FOR MOODANALYZER SAD,HAPPY & OTHER MOOD PROGRAM \n2 FOR MOODANALYZER INVALID INPUT PROGRAM\n3 FOR CUSTOM EXCEPTION PROGRAM\n4 FOR MOODANALYZER RETURN OBJECT PROGRAM\n5 FOR MOODANALYZER RETURN OBJECT WITH CONSTRUCTOR USING REFLECTION PROGRAM\n6 FOR MOODANALYZER INVOKE PROGRAM\n ");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -30,6 +30,12 @@ switch (num)
         break;
 
     case 5:
+        MoodAnalyze3 Moodobj5 = new MoodAnalyze3();
+        string result5 = Moodobj5.analyzeMood3();
+        Console.WriteLine(result5);
+        break;
+
+    case 6:
         MoodAnalyze3 Moodobj5 = new MoodAnalyze3();
         string result5 = Moodobj5.analyzeMood3();
         Console.WriteLine(result5);
