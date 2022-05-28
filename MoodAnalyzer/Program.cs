8d79714312d0368ed1e813bd79eaf23e66e001c9
AnkushRawat <ankushrwt344@gmail.com>
2022-05-28 15:38:43 +0530

    Uploading MoodanalyzerReturnObjectJUsingReflection file in Uc5 branch

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MoodAnalyzer;
 Console.WriteLine("WELCOME TO MOOD ANALYZER PROGRAM ");
-Console.WriteLine("Enter \n1 FOR MOODANALYZER SAD,HAPPY & OTHER MOOD PROGRAM \n2 FOR MOODANALYZER INVALID INPUT PROGRAM\n3 FOR CUSTOM EXCEPTION PROGRAM\n4 FOR MOODANALYZER RETURN OBJECT PROGRAM\n ");
+Console.WriteLine("Enter \n1 FOR MOODANALYZER SAD,HAPPY & OTHER MOOD PROGRAM \n2 FOR MOODANALYZER INVALID INPUT PROGRAM\n3 FOR CUSTOM EXCEPTION PROGRAM\n4 FOR MOODANALYZER RETURN OBJECT PROGRAM\n5 FOR MOODANALYZER RETURN OBJECT WITH CONSTRUCTOR USING REFLECTION PROGRAM\n ");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -27,5 +27,11 @@ switch (num)
         MoodAnalyze3 Moodobj4 = new MoodAnalyze3();
         string result4 = Moodobj4.analyzeMood3();
         Console.WriteLine(result4);
+        break;
+
+    case 5:
+        MoodAnalyze3 Moodobj5 = new MoodAnalyze3();
+        string result5 = Moodobj5.analyzeMood3();
+        Console.WriteLine(result5);
         break;
 }
